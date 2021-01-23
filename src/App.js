a45e28ef75dddeb7c7d38ed0d9c52f8516a5dad6
Sean Shubin <sshubin@cj.com>
2021-01-22 22:47:06 -0800

    profile handlers

--- src/App.js
+++ src/App.js
@@ -255,13 +255,84 @@ const mapProfileDispatchToProps = {
     fetchProfilesRequest,
     addProfileRequest,
     fetchProfilesSuccess,
-    profileNameChanged: name => ({type: PROFILE_NAME_CHANGED, name}),
+    profileNameChanged,
     deleteProfileRequest
 }
 
 const ConnectedProfile = connect(mapProfileStateToProps, mapProfileDispatchToProps)(Profile)
 
-const Task = () => <div>Task Component</div>
+const TaskListItem = ({task, updateTaskRequest}) => {
+    const completeClass = task.complete ? 'complete' : 'in-progress';
+    const onClick = () => {
+        const newTask = {...task, complete: !task.complete}
+        updateTaskRequest(newTask)
+    }
+    return <span className={completeClass} onClick={onClick}>
+        {task.name}
+    </span>
+}
+
+const TaskList = ({tasks, updateTaskRequest}) => {
+    const createElement = task =>
+        <TaskListItem key={task.id}
+                      task={task}
+                      updateTaskRequest={updateTaskRequest}/>
+    const taskElements = R.map(createElement, tasks)
+    return <div className={'elements'}>
+        {taskElements}
+    </div>
+}
+
+const AddTask = ({profile, taskName, taskNameChanged, addTaskRequest}) => {
+    const onKeyUp = event => {
+        if (R.trim(taskName) === '') return
+        if (event.key !== 'Enter') return
+        const task = {profileId: profile.id, name: taskName, complete: false}
+        addTaskRequest(task)
+    }
+    const onChange = event => {
+        taskNameChanged(event.target.value)
+    }
+    return <input value={taskName}
+                  autoFocus={true}
+                  placeholder={'task name'}
+                  onKeyUp={onKeyUp}
+                  onChange={onChange}/>
+}
+
+const Task = ({
+                  profile,
+                  tasks,
+                  taskName,
+                  errors,
+                  taskNameChanged,
+                  addTaskRequest,
+                  updateTaskRequest,
+                  deleteTasksRequest
+              }) => {
+    const header = `${tasks.length} ${pluralize({
+        quantity: tasks.length,
+        singular: 'task',
+        plural: 'tasks'
+    })} in profile ${profile.name}`
+    const onClickClearComplete = () => {
+        const taskIsComplete = task => task.complete
+        const completedTasks = R.filter(taskIsComplete, tasks)
+        const completedTaskIds = R.map(R.prop('id'), completedTasks)
+        deleteTasksRequest(completedTaskIds)
+    }
+    return <div className={'Task'}>
+        <h2>{header}</h2>
+        <AddTask profile={profile}
+                 taskName={taskName}
+                 taskNameChanged={taskNameChanged}
+                 addTaskRequest={addTaskRequest}/>
+        <TaskList tasks={tasks}
+                  updateTaskRequest={updateTaskRequest}/>
+        <button onClick={onClickClearComplete}>Clear Completed</button>
+        <a href={'/profile'}>Profiles</a>
+    </div>
+}
 
 const Summary = ({profileCount, taskCount}) =>
     <div className={"Summary"}>
@@ -274,7 +345,7 @@ const mapSummaryStateToProps = state => ({
     taskCount: R.view(lensTaskCount, state)
 })
 
-const mapSummaryDispatchToProps = dispatch => ({})
+const mapSummaryDispatchToProps = {}
 
 const ConnectedSummary = connect(mapSummaryStateToProps, mapSummaryDispatchToProps)(Summary)
 
@@ -294,7 +365,7 @@ const mapNavigationStateToProps = state => ({
     page: R.view(lensPage, state)
 })
 
-const mapNavigationDispatchToProps = dispatch => ({})
+const mapNavigationDispatchToProps = {}
 
 const ConnectedNavigation = connect(mapNavigationStateToProps, mapNavigationDispatchToProps)(Navigation)
 
